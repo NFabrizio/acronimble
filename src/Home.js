e9870f14fb1e48a6c5b03eec6c988b45882b5001
Nathan Kluth <nlkluth@gmail.com>
2018-04-18 11:46:58 -0600

    adding auth0

---
+++ src/Home.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
+  render() {
+    const { isAuthenticated } = this.props.auth;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          {isAuthenticated()? <button onClick={this.logout}>bye</button> : <button onClick={this.login}>hi</button>}
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
