e5e1a76004787c27b50015b2e6d58ad7489a320f
tsrahm <tory.rahm@pearson.com>
2018-04-18 15:48:51 -0600

    More ui stuff and it still is not broken

--- src/AcronymList.js
+++ src/AcronymList.js
@@ -1,37 +1,72 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Card, { CardActions, CardHeader, CardContent } from 'material-ui/Card';
+import Chip from 'material-ui/Chip';
 import Badge from 'material-ui/Badge';
 import IconButton from 'material-ui/IconButton';
-// import ThumbsUpIcon from 'material-ui/svg-icons/action/thumb-up';
 import ThumbsUpIcon from '@material-ui/icons/ThumbUp';
+import { withStyles } from 'material-ui/styles';
 
-const AcronymList = () => (
-  <div className="acronym-list">
-    <Card className="acronym-card">
-      <CardHeader
-        title={<h3>PI</h3>}
-        className="card-header"
-        style={{padding: 8}}
-      />
-      <CardContent>
-        Personal Information
-      </CardContent>
-      <CardContent>
-        User's personal information!
-      </CardContent>
-      <CardActions>
-        <Badge
-          badgeContent={10}
-          color="secondary"
-          badgeStyle={{top: 18, right: 18}}
-        >
-          <IconButton tooltip="Like">
-            <ThumbsUpIcon />
-          </IconButton>
-        </Badge>
-      </CardActions>
-    </Card>
-  </div>
-);
+const styles = {
+  badge: {
+    top: -6,
+    right: -6
+  },
+  title: {
+    display: 'inline'
+  },
+  subheader: {
+    display: 'inline',
+    float: 'right'
+  }
+};
 
-export default AcronymList;
+const acronymCategories = (list) => {
+  return list.map((item) => {
+    return (
+      <Chip label={item} style={{marginLeft: 10}} />
+    );
+  });
+};
+
+const AcronymList = (props) => {
+  return props.list.map((item) => {
+    return (
+      <div className="acronym-list">
+        <Card className="acronym-card">
+          <CardHeader
+            title={<span style={{display: 'inline'}}>{item.title}</span>}
+            subheader={acronymCategories(item.category)}
+            style={{padding: 10, paddingLeft: 16, backgroundColor: '#bbb'}}
+            classes={{title: props.classes.title, subheader: props.classes.subheader}}
+          >
+          </CardHeader>
+          <CardContent style={{fontSize: 18, paddingBottom: 10}}>
+            {item.fullName}
+          </CardContent>
+          <CardContent style={{fontSize: 14, paddingTop: 0, paddingBottom: 24}}>
+            {item.description}
+          </CardContent>
+          <CardActions style={{position: 'absolute', top: 70, right: 8}}>
+            <Badge
+              badgeContent={10}
+              color="secondary"
+              classes={{badge: props.classes.badge}}
+            >
+              <IconButton tooltip="Like" >
+                <ThumbsUpIcon style={{fontSize: 28}} />
+              </IconButton>
+            </Badge>
+          </CardActions>
+        </Card>
+      </div>
+    );
+  });
+};
+
+AcronymList.propTypes = {
+  children: PropTypes.node,
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(AcronymList);
--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import './App.css';
 import AcronymList from './AcronymList';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+const acronyms = [
+  {
+    title: 'PI',
+    fullName: 'Personal Information',
+    description: "User's personal information!",
+    category: ['technology', 'security']
+  },
+  {
+    title: 'GLP',
+    fullName: 'Global Learning Platform',
+    description: "The future of Pearson!",
+    category: ['technology', 'leadership']
+  }
+];
+
 class App extends Component {
   render() {
     return (
@@ -11,9 +26,10 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">acronimble</h1>
+            <h1 className="App-title" style={{marginBottom: 0}}>acronymble</h1>
+            <span className="App-title" style={{fontSize: 14}}>it's fun</span>
           </header>
-          <AcronymList />
+          <AcronymList list={acronyms} />
         </div>
       </MuiThemeProvider>
     );
