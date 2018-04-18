deb1a3da59c462d7f117366a084694c9ccc30b92
NFabrizio <nick.fabrizio@pearson.com>
2018-04-18 10:19:50 -0600

    Adds test data

---
+++ src/testDataCategories.js
@@ -0,0 +1,20 @@
+const testDataCategories = [
+ {
+   id: 'xyz',
+   categoryName: 'industry terminology'
+ },
+ {
+   id: 'zyx',
+   categoryName: 'business terminology'
+ },
+ {
+   id: 'abc',
+   categoryName: 'company terminology'
+ },
+ {
+   id: 'def',
+   categoryName: 'product'
+ }
+];
+
+module.exports = testDataCategories;
