2d8d0ff1511ecfe1f47416f1b13b4dc6d043897a
NFabrizio <nick.fabrizio@pearson.com>
2018-04-18 11:08:33 -0600

    Adds MongoDB

--- server/server.js
+++ server/server.js
@@ -1,7 +1,26 @@
-const express = require('express');
 const bodyParser = require('body-parser')
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 const path = require('path');
+
 const app = express();
+const port = process.env.PORT || 8080;
+const url = 'mongodb://localhost:27017';
+let db;
+const dbName = 'acronimble';
+
+MongoClient.connect(url, function(err, client) {
+  if(err) {
+    return console.log(err);
+  }
+
+  console.log("Database connected successfully to server");
+
+  db = client.db(dbName);
+
+  client.close();
+});
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/ping', function (req, res) {
@@ -12,4 +31,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(port, function() {
+ console.log(`AcroNimble server running on port ${port}`);
+});
