1d3eb29e2cb879d692eb51ad250e331632a1e31a
NFabrizio <nick.fabrizio@pearson.com>
2018-04-19 11:13:01 -0600

    Adds navigation styling

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,8 @@
-import { AppBar } from 'material-ui';
-import React, { Component } from 'react';
+import Button from 'material-ui/Button';
 import { Link } from 'react-router-dom';
-import logo from './assets/pearson-logo.png';
+import Menu, { MenuItem } from 'material-ui/Menu';
+import React, { Component } from 'react';
+import pearsonLogo from './assets/pearson-logo.png';
 import './App.css';
 import axios from 'axios';
 import logo from './logo.svg';
@@ -11,20 +12,6 @@ import AcronymList from './AcronymList';
 import AcronymPage from './AcronymPage';
 
 const theme = createMuiTheme();
-const showExample = (userProfile, logout) => {
-  return (
-    <div className="profile-block">
-      {userProfile && <img src={userProfile.picture} style={{height: '50px', width: '50px' }} />}
-      <div>
-        <Link to="/profile">View Profile</Link>
-      </div>
-      <div>
-        <Link to="/new">New Acronym</Link>
-      </div>
-      <button onClick={logout}>Logout</button>
-    </div>
-  );
-};
 
 const acronyms = [
   {
@@ -53,14 +40,23 @@ class Home extends Component {
     super(props);
 
     this.state = {
-      showAcronym: false,
-      acronymTitle: ''
+      acronymTitle: '',
+      anchorElement: null,
+      showAcronym: false
     }
 
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
     this.showCard = this.showCard.bind(this);
   }
+
+  handleClick = event => {
+    this.setState({ anchorElement: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorElement: null });
+  };
 
   showCard(acronymTitle) {
     this.setState({
@@ -85,6 +81,42 @@ class Home extends Component {
     return <AcronymPage item={acronym} />
   }
 
+  showExample() {
+    const { anchorElement } = this.state;
+    const userProfile = this.props.auth.userProfile;
+    return (
+      <div>
+        {userProfile && <img src={userProfile.picture} style={{height: '50px', width: '50px', display: 'block', margin: '0 auto' }} />}
+        <Button
+            aria-owns={anchorElement ? 'simple-menu' : null}
+            aria-haspopup="true"
+            onClick={this.handleClick}
+            style={{ display: 'block', textAlign: 'center' }}
+          >
+            <div style={{ width: '35px', height: '5px', backgroundColor: 'white', margin: '6px auto', borderRadius: '3px' }}></div>
+            <div style={{ width: '35px', height: '5px', backgroundColor: 'white', margin: '6px auto', borderRadius: '3px' }}></div>
+            <div style={{ width: '35px', height: '5px', backgroundColor: 'white', margin: '6px auto', borderRadius: '3px' }}></div>
+        </Button>
+        <Menu
+            id="simple-menu"
+            anchorEl={anchorElement}
+            open={Boolean(anchorElement)}
+            onClose={this.handleClose}
+          >
+          <MenuItem onClick={this.handleClose}>
+            <Link to="/profile" style={{ textDecoration: 'none', color: 'black' }}>View Profile</Link>
+          </MenuItem>
+          <MenuItem onClick={this.handleClose}>
+            <Link to="/new" style={{ textDecoration: 'none', color: 'black' }}>New Acronym</Link>
+          </MenuItem>
+          <MenuItem onClick={this.logout}>
+            Logout
+          </MenuItem>
+        </Menu>
+      </div>
+    );
+  };
+
   login() {
     this.props.auth.login();
   }
@@ -99,18 +131,22 @@ class Home extends Component {
     return (
       <MuiThemeProvider theme={theme}>
         <div className="App">
-          <AppBar className="App-header">
-            {isAuthenticated() ? showExample(this.props.auth.userProfile, this.logout) : <button onClick={this.login}>hi</button>}
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1
-              className="App-title"
-              style={{marginBottom: 0, cursor: 'pointer'}}
-              onClick={() => this.goHome()}
-            >
-              acronymble
-            </h1>
-            <span className="App-title" style={{fontSize: 14}}>its fun</span>
-          </AppBar>
+          <header className="App-header">
+            <div className="app-info">
+              <img src={pearsonLogo} className="App-logo" alt="logo" />
+              <h1
+                className="App-title"
+                style={{marginBottom: 0, cursor: 'pointer'}}
+                onClick={() => this.goHome()}
+              >
+                acronymble
+              </h1>
+              <span className="App-title" style={{fontSize: 14}}>its fun</span>
+            </div>
+            <div className="profile-block">
+              {isAuthenticated() ? this.showExample() : <button onClick={this.login}>Login</button>}
+            </div>
+          </header>
           <div className="acronym-container">
             {this.renderView()}
           </div>
