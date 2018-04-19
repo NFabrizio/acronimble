73b1f11b09792a1f321a2165d2cffe1ee5a30ffd
tsrahm <tory.rahm@pearson.com>
2018-04-19 12:37:01 -0600

    added conditionals

--- src/AcronymPage.js
+++ src/AcronymPage.js
@@ -52,7 +52,6 @@ class AcronymPage extends React.Component {
 
   render() {
     const item = this.state.acronym;
-
     if (this.state.loading) {
       return 'loading...'
     }
@@ -78,7 +77,7 @@ class AcronymPage extends React.Component {
           {item.definitions && item.definitions[0].description}
         </CardContent>
         <div style={{display: 'grid', gridTemplateColumns: '1fr 3fr'}}>
-          <AcronymLike style={{gridArea: '1/1/auto/auto'}} likes={item.definitions[0].likes.length} />
+          <AcronymLike style={{gridArea: '1/1/auto/auto'}} likes={item.definitions[0].likes && item.definitions[0].likes.length} />
           <CardContent style={{fontSize: 14, padding: '30px 24px 16px', gridArea: '1/2/auto/auto', textAlign: 'right'}}>
             Submitted by tory
           </CardContent>
