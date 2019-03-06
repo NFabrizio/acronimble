5c75d784085e5259510022939553a9455054fe15
Nathan Kluth <nlkluth@gmail.com>
2019-03-06 12:01:15 -0700

    remove unused import

--- src/NoResultsPage.js
+++ src/NoResultsPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import { withStyles } from '@material-ui/styles';
