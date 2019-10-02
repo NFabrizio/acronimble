7325117a34c64c407455b9cf8c8b8ca976b325ce
tsrahm <tory.rahm@pearson.com>
2019-10-02 13:34:59 -0600

    Add Sentry to server and UI

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+Sentry.init({ dsn: 'https://7a6c8e8216504f07a565c7bd483f233b@sentry.io/1768395' });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
