2d181be2f321e37fb2b83cfe4b956baa78714bd7
tsrahm <tory.rahm@pearson.com>
2019-10-14 14:37:16 -0600

    Add sentry/browser

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({dsn: "https://7a6c8e8216504f07a565c7bd483f233b@sentry.io/1768395"});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
