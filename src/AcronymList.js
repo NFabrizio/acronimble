6c6153dd89e1788e1037014f4aace26b3d81f08f
NFabrizio <nick.fabrizio@pearson.com>
2019-11-22 09:34:13 -0700

    allow-duplicate-acronym-definitions Updates AcronymList to show the definition and name of the acronym with the most likes

--- src/AcronymList.js
+++ src/AcronymList.js
@@ -35,6 +35,22 @@ const isLiked = (likesIds, definitionId) => {
   });
 };
 
+const mostPopularDefinition = (definitions = []) => {
+  const mostPopular = definitions.reduce((previous, current) =>
+    previous.likes.length > current.likes.length ? previous : current
+  );
+  return (
+    <div style={{ padding: '16px 24px' }}>
+      <CardContent style={{ fontSize: 18, padding: '0 40px 10px 0' }}>
+        {mostPopular.name}
+      </CardContent>
+      <CardContent style={{ fontSize: 14, padding: '0 40px 0 0' }}>
+        {mostPopular.description}
+      </CardContent>
+    </div>
+  );
+};
+
 const AcronymList = props => {
   const { list = [], like, isAuthenticated, likesIds } = props;
 
@@ -43,6 +59,13 @@ const AcronymList = props => {
   }
 
   return list.map(item => {
+    const mostPopularDefinition = item.definitions.reduce((previous, current) => {
+      const prevLikesLength = (previous && previous.likes && previous.likes.length) || 0;
+      const currLikesLength = (current && current.likes && current.likes.length) || 0;
+
+      return prevLikesLength > currLikesLength ? previous : current;
+    });
+
     return (
       <Card className="acronym-card" key={item.acronym}>
         <Link to={`/acronyms/${item._id}`} style={{ color: 'black', textDecoration: 'none' }}>
@@ -54,20 +77,20 @@ const AcronymList = props => {
           ></CardHeader>
           <div style={{ padding: '16px 24px' }}>
             <CardContent style={{ fontSize: 18, padding: '0 40px 10px 0' }}>
-              {item.definitions[0].name}
+              {mostPopularDefinition.name}
             </CardContent>
             <CardContent style={{ fontSize: 14, padding: '0 40px 0 0' }}>
-              {item.definitions[0].description}
+              {mostPopularDefinition.description}
             </CardContent>
           </div>
         </Link>
         <AcronymLike
           style={{ position: 'absolute', top: 70, right: 14 }}
           like={like}
-          likes={item.definitions[0].likes || []}
-          definitionId={item.definitions[0].id}
+          likes={mostPopularDefinition.likes || []}
+          definitionId={mostPopularDefinition.id}
           itemId={item._id}
-          liked={isLiked(likesIds, item.definitions[0].id)}
+          liked={isLiked(likesIds, mostPopularDefinition.id)}
           isAuthenticated={isAuthenticated}
         />
       </Card>
